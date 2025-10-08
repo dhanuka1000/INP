82855d524f33ad11c917065fa6e76c691a92ffb8
Dhanuka lakshan <ldhanuka173@gmail.com>
2025-10-08 11:45:43 +0530

    Client side

--- SocketProgramming/src/main/java/lk/ijse/Client.java
+++ SocketProgramming/src/main/java/lk/ijse/Client.java
@@ -1,7 +1,21 @@
 package lk.ijse;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client {
     public static void main(String[] args) {
-
+        try{
+            Socket remoteSocket = new Socket("localhost", 8888);
+            DataOutputStream out = new DataOutputStream(remoteSocket.getOutputStream());
+            out.writeUTF("Hello Server");
+            out.flush();
+            out.close();
+            remoteSocket.close();
+        }
+        catch(IOException e){
+            throw new RuntimeException(e);
+        }
     }
 }
