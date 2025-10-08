0f653ea5b9e733ba1470af05c6f55a9a8423afc1
Dhanuka lakshan <ldhanuka173@gmail.com>
2025-10-08 11:31:30 +0530

    server side

--- SocketProgramming/src/main/java/lk/ijse/Server.java
+++ SocketProgramming/src/main/java/lk/ijse/Server.java
@@ -1,7 +1,28 @@
 package lk.ijse;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
     public static void main(String[] args) {
 
+        try {
+            ServerSocket serverSocket = new ServerSocket(8888);
+            System.out.println("Server Started");
+
+            Socket socket=serverSocket.accept();
+            System.out.println("Client Accepted");
+
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+
+            String massage = dataInputStream.readUTF();
+            System.out.println("Client :"+massage);
+            socket.close();
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
